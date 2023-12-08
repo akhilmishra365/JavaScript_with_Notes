cd90d092aa76fe17ae27c704d4174094954daf36
--replace-all <akhilmishra1676@gmail.com>
2023-12-09 00:13:46 +0530

    error

--- 02_Basics/02_Scope.js
+++
@@ -1,9 +0,0 @@
-// Global scope
-let globalVariable = "I am in global scope";
-
-function globalFunction() {
-  console.log(globalVariable); // Accessible within the function
-}
-
-console.log(globalVariable); // Accessible outside the function
-globalFunction();
