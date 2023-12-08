c3f53e1b6e235185dc750c6dc438958cc7c794c6
--replace-all <akhilmishra1676@gmail.com>
2023-12-09 00:21:51 +0530

    Basics Of Javascript Part 2
    Functions , Scope , Arrow Fucnction , Javascript Execution Context , all codes with Notes

---
+++ 02_BASICS/02_Scope.js
@@ -0,0 +1,9 @@
+// Global scope
+let globalVariable = "I am in global scope";
+
+function globalFunction() {
+  console.log(globalVariable); // Accessible within the function
+}
+
+console.log(globalVariable); // Accessible outside the function
+globalFunction();
